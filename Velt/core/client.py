8c9e6c886df768f8d446e4a9d1eef7fdea1cff0f
Harikeshav R <r.harikeshav@gmail.com>
2023-07-11 21:30:29 +0530

    Create connect functionality

--- Velt/core/client.py
+++ Velt/core/client.py
@@ -9,3 +9,17 @@ class Client:
     def __init__(self, host: str, port: int) -> None:
         self.host = socket.gethostbyname(host)
         self.port = port
+
+        self.tcp = TCP(self.connect())
+
+    def connect(self) -> socket.socket:
+        while True:
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((self.host, self.port))
+                print("Connected!")
+
+                return s
+
+            except socket.error:
+                time.sleep(3)
